69d0c7d68070f1a1943498543ded514f551afeb1
aldenpower <anderson_afsl@hotmail.com>
2022-01-25 12:40:24 -0300

    config experiment

--- helicopterexerc/eli.r
+++ helicopterexerc/eli.r
@@ -6,7 +6,7 @@ getwd()
 
 protot = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/protot.csv")
 protot
-# Não foi considerada a mesma altura de lançamento para cada operador
+# Não foi considerada a mesma altura de lançamento para cada operador e a mao foi colocada no clipe
 elidf1 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf1.csv")
 elidf1
 # RR test
@@ -16,7 +16,7 @@ shapiro.test(elidf1$ft)
 
 # SEGUNDO EXPERIMENTO
 
-# Foi considerada a mesma altura de lançamento de 1.5m
+# Foi considerada a mesma altura de lançamento de 1.5m medida no momento do lancamento e a mao foi colocada no clipe
 elidf2 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf2.csv")
 
 shapiro.test(elidf2$ft)
@@ -29,6 +29,7 @@ boxplot(elidf2$ft ~ elidf2$op)
 # TERCEIRO EXPERIMENTO
 
 # Foram considerados 5 runs compostos pela media entre 3 lancamentos para 4 prototipos com medidas iguais e altura de lancamento de 1.5m
+# medida no momento do lancamento e mao colocada no clipe
 elidf3 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf3.csv")
 elidf3$'ft' = (elidf3$'ft1' + elidf3$'ft2' + elidf3$'ft3') / 3
 elidf3
