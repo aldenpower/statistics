69e027161118fea3e0174d2f5e8294f873e64fbe
aldenpower <anderson_afsl@hotmail.com>
2022-01-26 08:21:04 -0300

    config experiment

--- helicopterexerc/eli.r
+++ helicopterexerc/eli.r
@@ -1,26 +1,32 @@
 library("SixSigma")
 
-# PRIMEIRO EXPERIMENTO
+# Prototipos utilizados
 protot = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/protot.csv")
+
+# Resumo de experimentos
+experiments = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/experiments.csv")
+
+# PRIMEIRO EXPERIMENTO
 elidf1 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf1.csv")
-ss.rr(var = ft, part = pr, appr = op, data = elidf1)
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf1)
 # SEGUNDO EXPERIMENTO
 elidf2 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf2.csv")
-# RR test
-ss.rr(var = ft, part = pr, appr = op, data = elidf2)
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf2)
 # TERCEIRO EXPERIMENTO
 elidf3 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf3.csv")
-elidf3$'ft' = (elidf3$'ft1' + elidf3$'ft2' + elidf3$'ft3') / 3
-# RR test
-ss.rr(var = ft, part = pr, appr = op, data = elidf3)
+elidf3$'flight.time' = (elidf3$'flight.time.1' + elidf3$'flight.time.2' + elidf3$'flight.time.3') / 3
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf3)
 # QUARTO EXPERIMENTO
 elidf4 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf4.csv")
-elidf4$'flight' = (elidf4$'flight.time.1' + elidf4$'flight.time.2' + elidf4$'ft3') / 3
-# RR test
-ss.rr(var = ft, part = pr, appr = op, data = elidf4)
+elidf4$'flight.time' = (elidf4$'flight.time.1' + elidf4$'flight.time.2' + elidf4$'flight.time.3') / 3
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf4)
 # QUINTO EXPERIMENTO
 elidf5 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf5.csv")
+elidf5$'flight.time' = (elidf5$'flight.time.1' + elidf5$'flight.time.2' + elidf5$'flight.time.3') / 3
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf5)
 # SEXTO EXPERIMENTO
 elidf6 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf6.csv")
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf6)
 # SETIMO EXPERIMENTO
 elidf7 = read.csv("/home/mr-alden/reps/statistics/helicopterexerc/elidf7.csv")
+ss.rr(var = flight.time, part = prototipo, appr = operador, data = elidf7)
